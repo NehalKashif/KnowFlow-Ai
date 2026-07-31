f3a79595c5f69dd181708067ec7b1f6437ad78c1
Nehal Kashif <nehalkashif112@gmail.com>
2026-07-31 16:37:54 +0500

    Testing of modules

---
+++ backend/tests/test_loader.py
@@ -0,0 +1,22 @@
+from rag.loader import DocumentLoader
+from rag.splitter import DocumentSplitter
+from rag.embeddings import EmbeddingManager
+from rag.vector_store import VectorStore
+
+loader = DocumentLoader()
+
+documents = loader.load_document("data/AI_Research_paper.pdf")
+
+print(f"Loaded {len(documents)} pages\n")
+
+spliter = DocumentSplitter(chunk_size=1000, chunk_overlap=200)
+chunks = spliter.split_documents(documents)
+print(f"chunks length: {len(chunks)}")
+
+generator = EmbeddingManager()
+chunks_embeddings = generator.generate_embeddings(chunks)
+
+print(len(chunks_embeddings))
+
+vectorstore = VectorStore()
+vectorstore.add_documents(chunks, chunks_embeddings)
