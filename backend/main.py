da9857411d1032935ba1bd2342cbc946204c36bf
Nehal Kashif <nehalkashif112@gmail.com>
2026-08-09 21:53:00 +0500

    Registeration tested

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,6 @@
 import logging
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
 from api.routes import router
 from api.auth import router as auth_router
 
@@ -10,6 +10,18 @@ myapp = FastAPI(
     title="KnowFlow AI",
     description="Personal RAG Assistant API",
     version="1.0.0"
+)
+
+myapp.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
+        "http://192.168.56.1:3000",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 # Chat & Upload Routes
